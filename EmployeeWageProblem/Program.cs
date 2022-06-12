65f33ae4ae8daeae214856acd2b73e9aa206a707
Siddhant <santoshshrivastava39@gmail.com>
2022-06-12 13:09:45 +0530

    Welcome Message

---
+++ EmployeeWageProblem/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace EmployeeWageProblem
+{
+    internal class Program
+    {
+
+        public const int IsPartTime = 2;
+        public const int IsFullTime = 1;
+        public const int IsAbsent = 0;
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+
+
+            
+        }
+    }
+}
