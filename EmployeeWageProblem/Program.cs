e387c9a83f1068000e71ce845bed6009301c3527
Siddhant <santoshshrivastava39@gmail.com>
2022-06-12 13:12:29 +0530

    Added UC5 Solution

--- EmployeeWageProblem/Program.cs
+++ EmployeeWageProblem/Program.cs
@@ -13,8 +13,37 @@ namespace EmployeeWageProblem
 
             Console.WriteLine("Welcome to Employee Wage Computation Program");
 
+            int Totalwage = 0;
+            int WagePerHr = 20;
+            int PartTimeHr = 4;
+            int FullTimeHr = 8;
+            int NumberOfDaysPerMonth = 20;
 
-            
+            for (int i = 0; i < NumberOfDaysPerMonth; i++)
+            {
+                Random random = new Random();
+                int number = random.Next(0, 3);
+                switch (number)
+                {
+                    case IsFullTime:
+                        Console.WriteLine("Employee is Full Time");
+                        Totalwage = Totalwage + WagePerHr * FullTimeHr;
+                        break;
+                    case IsPartTime:
+                        Console.WriteLine("Employee is working part time");
+                        Totalwage = Totalwage + WagePerHr * PartTimeHr;
+
+                        break;
+                    case IsAbsent:
+                        Console.WriteLine("Employee is Absent");
+                        break;
+
+
+                }
+                Console.WriteLine("Employee's Total Wage is" + Totalwage);
+
+            }
+
         }
     }
 }
